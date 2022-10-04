369a6c4173a0b615b072c550f00ee13f23bc8817
NgoManhSon <ngomanhson2004txpt@gmail.com>
2022-10-04 12:16:08 +0700

    Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ClassroomComponent} from "./classroom/classroom.component";
+import {StudentComponent} from "./student/student.component";
+import {TeacherComponent} from "./teacher/teacher.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ClassroomComponent, StudentComponent, TeacherComponent
   ],
   imports: [
     BrowserModule
